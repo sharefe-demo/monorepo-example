0213a302692d787eecae607c07937975953d9c8c
afe <nomail@local>
2022-02-21 20:18:36 +0800

    chore: show replace log

--- packages/repo-c/index.js
+++ packages/repo-c/index.js
@@ -3,8 +3,8 @@ const pkg = require('./package.json')
 
 const templateFileName = 'repo-c.js'
 
+console.log('\nReplacing the file with template... ')
 const template = fs.readFileSync('./src/' + templateFileName, 'utf-8')
-
 const replaced = template.replace('{{VERSION}}', pkg.version)
-
 fs.writeFileSync('./lib/' + templateFileName, replaced, 'utf-8')
+console.log('Done!\n\n')
